34956f6c65aa4131fb31a5b795b6d28bb3490cba
Thomas Rueckstiess <tr@capslock.local>
2012-12-03 13:22:33 +1100

    more comments in presplit.

--- mutil.py
+++ mutil.py
@@ -39,39 +39,41 @@ def extractDateTime(line):
 
 def presplit(host, database, collection, shardkey, disableBalancer=True):
     """ get information about the number of shards, then split chunks and distribute over shards. Currently assumes shardkey to be ObjectId/uuid (hex). """
+    
     con = Connection(host)
     namespace = '%s.%s'%(database, collection)
 
     # disable balancer
     con['config']['settings'].update({'_id':"balancer"}, {'$set':{'stopped': True}}, upsert=True)
 
-    # enable sharding on database if not enabled yet
+    # enable sharding on database if not yet enabled
     db_info = con['config']['databases'].find_one({'_id':database})
     if not db_info or db_info['partitioned'] == False:
         con['admin'].command(SON({'enableSharding': database}))
 
-    # shard collection
+    # shard collection if not yet sharded
     coll_info = con['config']['collections'].find_one({'_id':namespace})
     if coll_info and not coll_info['dropped']:
+        # if it is sharded already, quit. something is not right.
         print "collection already sharded."
         return
     else:
         con[database][collection].ensure_index(shardkey)
         con['admin'].command(SON({'shardCollection': namespace, 'key': {shardkey:1}}))
 
-    # pre-split
+    # get shard number and names and calculate split points
     shards = list(con['config']['shards'].find())
     shard_names = [s['_id'] for s in shards]
-
     split_interval = 16 / len(shards)
     split_points = [hex(s).lstrip('0x') for s in range(split_interval, len(shards)*split_interval, split_interval)]
     
+    # pre-splitting commands
     for s in split_points:
         con['admin'].command(SON([('split',namespace), ('middle', {shardkey: s})]))
     
     split_points = ['MinKey'] + split_points
-    print split_points
 
+    # move chunks to shards (catch the one error where the chunk resides on that shard already)
     for i,s in enumerate(split_points):
         try:
             print [('moveChunk',namespace), ('find', {shardkey: s}), ('to', shard_names[i])]
@@ -82,5 +84,6 @@ def presplit(host, database, collection, shardkey, disableBalancer=True):
 
 
 if __name__ == '__main__':
+    # test presplitting function
     presplit('capslock.local:27024', 'test', 'mycol', 'my_id')
 
