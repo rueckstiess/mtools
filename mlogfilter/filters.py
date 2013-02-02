fdf17df869f81f5ed131a5d4455e25f37716fc24
Thomas Rueckstiess <thomas@10gen.com>
2013-02-02 23:08:44 +1100

    fixed bug where --slow was always enabled after the last change.

--- mlogfilter/filters.py
+++ mlogfilter/filters.py
@@ -67,14 +67,18 @@ class SlowFilter(BaseFilter):
         duration is longer than the specified parameter in ms (default 1000).
     """
     filterArgs = [
-        ('--slow', {'action':'store', 'nargs':'?', 'default':1000, 'type':int, 'help':'only output lines with query times longer than SLOW ms (default 1000)'})
+        ('--slow', {'action':'store', 'nargs':'?', 'default':False, 'type':int, 'help':'only output lines with query times longer than SLOW ms (default 1000)'})
     ]
 
     def __init__(self, commandLineArgs):
         BaseFilter.__init__(self, commandLineArgs)
-        if 'slow' in self.commandLineArgs:
+        if 'slow' in self.commandLineArgs and self.commandLineArgs['slow'] != False:
             self.active = True
-            self.slowms = self.commandLineArgs['slow']
+            if self.commandLineArgs['slow'] == None:
+                self.slowms = 1000
+            else:
+                self.slowms = self.commandLineArgs['slow']
+            print self.slowms
 
     def accept(self, line):
         match = re.search(r'(\d+)ms$', line)
