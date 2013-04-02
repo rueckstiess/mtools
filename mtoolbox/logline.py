3737e65e7882ef9bfc76e91d9f3292e923261de7
Thomas Rueckstiess <thomas@10gen.com>
2013-04-02 12:08:08 +1100

    re-written LogLine to evaluate information lazily for performance improvement. can also detect date starting later in the log line.

--- mtoolbox/logline.py
+++ mtoolbox/logline.py
@@ -2,9 +2,13 @@ from datetime import datetime
 import re
 import json
 
-weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', \
-          'Oct', 'Nov', 'Dec']
+class DateTimeEncoder(json.JSONEncoder):
+    """ custom datetime encoder for json output. """
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        return json.JSONEncoder.default(self, obj)
+
 
 class LogLine(object):
     """ LogLine extracts information from a mongod/mongos log file line and 
@@ -15,88 +19,125 @@ class LogLine(object):
                       whitespace as split points
         datetime: a datetime object for the logline. For logfiles created with 
                   version 2.4+, it also contains micro-seconds
+        datetime_offset: the token position of the weekday field
         duration: the duration of a timed operation in ms
         connection: the connection id (e.g. "conn1234") as string
-        connection_pos: the token position of the connection
+        connection_offset: the token position of the connection
         operation: insert, update, remove, query, command, getmore, None
         namespace: the namespace of the operation, or None
         
         Certain operations also add the number of affected/scanned documents.
         If applicable, the following variables are also set, otherwise the 
         default is None: nscanned, ntoreturn, nreturned, ninserted, nupdated
+
+        For performance reason, all fields are evaluated lazily upon first
+        request.
     """
+
+    # datetime handler for json encoding
+    dthandler = lambda obj: obj.isoformat() if isinstance(obj, \
+        datetime) else None
+
+    weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', \
+        'Oct', 'Nov', 'Dec']
+
 
     def __init__(self, line_str, auto_parse=True):
         # remove line breaks at end of line_str
         self.line_str = line_str.rstrip('\n')
 
-        # datetime handler for json encoding
-        self.dthandler = lambda obj: obj.isoformat() if isinstance(obj, \
-            datetime) else None
+        self._split_tokens_calculated = False
+        self._split_tokens = None
 
-        # split into items (whitespace split)
-        self.split_tokens = self.line_str.split()
+        self._duration_calculated = False
+        self._duration = None
 
-        if auto_parse:
-            self._parse()
+        self._datetime_calculated = False
+        self._datetime = None
+        self._datetime_offset = None
 
-    def _parse(self):
-        # extract date and time
-        self._extract_datetime()
+        self._connection_calculated = False
+        self._connection = None
+        self._connection_offset = None
+
+        self._operation_calculated = False
+        self._operation = None
+        self._namespace = None
+
+        self._counters_calculated = False
+        self._nscanned = None
+        self._ntoreturn = None
+        self._nupdated = None
+        self._nreturned = None
+        self._ninserted = None
+
+
+    @property
+    def split_tokens(self):
+        """ splits string into tokens (lazy) """
+
+        if not self._split_tokens_calculated:
+            # split into items (whitespace split)
+            self._split_tokens = self.line_str.split()
+            self._split_tokens_calculated = True
+
+        return self._split_tokens
+
+
+    @property
+    def duration(self):
+        """ calculate duration if available (lazy) """
         
-        # extract duration from timed operations
-        self.duration = None
-        if len(self.split_tokens) > 0 and self.split_tokens[-1].endswith('ms'):
-            self.duration = int(self.split_tokens[-1][:-2])
-        
-        # extract connection id (conn####)
-        self.connection = None
-        for i, item in enumerate(self.split_tokens):
-            match = re.match(r'^\[(conn[^\]]*)\]$', item)
-            if match:
-                self.connection = match.group(1)
-                self.connection_pos = i
-                break
+        if not self._duration_calculated:
+            self._duration_calculated = True
 
-        # extract operation from connection logs
-        self.operation = None
-        self.namespace = None
-        if self.connection:
-            op = self.split_tokens[self.connection_pos+1]
-            if op in ['query', 'insert', 'update', 'remove', 'getmore', \
-                      'command']:
-                self.operation = op
-                self.namespace = self.split_tokens[self.connection_pos+2]
+            split_tokens = self.split_tokens
 
-                # extract n-values (if present)
-                keys = ['nscanned', 'ntoreturn', 'nreturned', 'ninserted', \
-                        'nupdated']
-                for i in self.split_tokens[self.connection_pos+2:]:
-                    for key in keys:
-                        if i.startswith('%s:'%key):
-                            vars(self)[key] = i.split(':')[-1]
-                            break
+            if len(split_tokens) > 0 and split_tokens[-1].endswith('ms'):
+                self._duration = int(split_tokens[-1][:-2])
+
+        return self._duration
 
 
-    def _extract_datetime(self):
-        self.datetime = None
+    @property
+    def datetime(self):
+        """ extract datetime if available (lazy) """
 
-        # check if there are enough tokens for datetime
-        if len(self.split_tokens) < 4:
-            return
+        if not self._datetime_calculated:
+            self._datetime_calculated = True
 
-        # log file structure: Wed Sep 05 23:02:26 ...
-        weekday, month, day, time = self.split_tokens[:4]
+            split_tokens = self.split_tokens
+
+            match_found = False
+            for offs in range(len(split_tokens)):
+                dt = self._match_datetime_pattern(split_tokens[offs:])
+                if dt:
+                    self._datetime = dt
+                    self._datetime_offset = offs
+
+        return self._datetime
+
+
+    def _match_datetime_pattern(self, tokens):
+        """ Helper method that takes a list of tokens and tries to match the 
+            datetime pattern at the beginning of the token list, i.e. the first
+            few tokens need to match [weekday], [month], [day], HH:MM:SS  
+            (potentially with milliseconds for mongodb version 2.4+). """
+        if len(tokens) < 4:
+            return None
+
+        weekday, month, day, time = tokens[:4]
         time_match = re.match(r'(\d{2}):(\d{2}):(\d{2})(\.\d{3})?', time)
 
         # check if it is a valid datetime
-        if not (weekday in weekdays and
-                month in months and
+        if not (weekday in self.weekdays and
+                month in self.months and
                 re.match(r'\d{1,2}', day) and
                 time_match):
-            return
+            return None
 
-        month = months.index(month)+1
+        month = self.months.index(month)+1
 
         # extract hours, min, sec, millisec from time string
         h, m, s, ms = time_match.groups()
@@ -110,42 +151,198 @@ class LogLine(object):
         # convert to microsec for datetime
         ms *= 1000
 
-        # TODO: special case if logfile is not from the current year
+        # assume this year. TODO: special case if logfile is not from current year
         year = datetime.now().year
 
         dt = datetime(int(year), int(month), int(day), int(h), int(m), \
                       int(s), ms)
 
-        self.datetime = dt
+        return dt
+
+
+    @property
+    def connection(self):
+        """ extract connection if available (lazy) """
+
+        if not self._connection_calculated:
+            self._connection_calculated = True
+
+            split_tokens = self.split_tokens
+
+            for offs, item in enumerate(split_tokens):
+                match = re.match(r'^\[(conn[^\]]*)\]$', item)
+                if match:
+                    self._connection = match.group(1)
+                    self._connection_offset = offs
+                    break
+  
+        return self._connection
+
+
+    @property
+    def operation(self):
+        """ extract operation (query, insert, update, remove, getmore, command) 
+            if available (lazy) """
+
+        if not self._operation_calculated:
+            self._operation_calculated = True
+            self._extract_operation_and_namespace()
+
+        return self._operation
+
+
+    @property
+    def namespace(self):
+        """ extract namespace if available (lazy) """
+
+        if not self._operation_calculated:
+            self._operation_calculated = True
+            self._extract_operation_and_namespace()
+
+        return self._namespace
+
+
+    def _extract_operation_and_namespace(self):
+        """ Helper method to extract both operation and namespace from a 
+            logline. It doesn't make sense to only extract one as they
+            appear back to back in the token list.
+        """
+
+        split_tokens = self.split_tokens
+
+        # trigger connection evaluation to get access to offset
+        if self.connection:
+            op = split_tokens[self._connection_offset + 1]
+
+            if op in ['query', 'insert', 'update', 'remove', 'getmore', 'command']:
+                self._operation = op
+                self._namespace = split_tokens[self._connection_offset + 2]
+
+
+
+    @property
+    def nscanned(self):
+        """ extract nscanned counter if available (lazy) """
+
+        if not self._counters_calculated:
+            self._counters_calculated = True
+            self._extract_counters()
+
+        return self._nscanned
+
+    @property
+    def ntoreturn(self):
+        """ extract ntoreturn counter if available (lazy) """
+
+        if not self._counters_calculated:
+            self._counters_calculated = True
+            self._extract_counters()
+
+        return self._ntoreturn
+
+
+    @property
+    def nreturned(self):
+        """ extract nreturned counter if available (lazy) """
+
+        if not self._counters_calculated:
+            self._counters_calculated = True
+            self._extract_counters()
+
+        return self._nreturned
+
+
+    @property
+    def ninserted(self):
+        """ extract ninserted counter if available (lazy) """
+
+        if not self._counters_calculated:
+            self._counters_calculated = True
+            self._extract_counters()
+
+        return self._ninserted
+
+    @property
+    def nupdated(self):
+        """ extract nupdated counter if available (lazy) """
+
+        if not self._counters_calculated:
+            self._counters_calculated = True
+            self._extract_counters()
+
+        return self._nupdated
+
+
+
+    def _extract_counters(self):
+        """ Helper method to extract counters like nscanned, nreturned, etc.
+            from the logline. 
+        """
+
+        # extract n-values (if present)
+        counters = ['nscanned', 'ntoreturn', 'nreturned', 'ninserted', \
+            'nupdated']
+
+        split_tokens = self.split_tokens
+
+        # trigger connection evaluation to get access to offset
+        if self.connection:
+            for token in split_tokens[self._connection_offset+2:]:
+                for counter in counters:
+                    if token.startswith('%s:'%counter):
+                        vars(self)['_'+counter] = int(token.split(':')[-1])
+                        break
+
+
+    def parse_all(self):
+        """ triggers the extraction of all information, which would usually
+            just be evaluated lazily. 
+        """
+        tokens = self.split_tokens
+        duration = self.duration
+        datetime = self.datetime
+        connection = self.connection
+        operation = self.operation
+        namespace = self.namespace
+        nscanned = self.nscanned
+        ntoreturn = self.ntoreturn
+        nreturned = self.nreturned
+        ninserted = self.ninserted
+        nupdated = self.nupdated
 
 
     def __str__(self):
+        """ default string conversion for a LogLine object. """
         output = ''
-        labels = ['datetime', 'operation', 'connection', 'namespace', \
-                  'nscanned', 'ntoreturn', 'nreturned', 'duration']
-        variables = vars(self)
+        labels = ['line_str', 'split_tokens', 'datetime', 'operation', \
+                  'connection', 'namespace', 'nscanned', 'ntoreturn',  \
+                  'nreturned', 'ninserted', 'nupdated', 'duration']
 
         for label in labels:
-            if not label in variables:
-                continue
-            output += '%s:'%label
-            output += str(variables[label])
-            output += " "
+            value = getattr(self, label, None)
+            if value != None:
+                output += '%s:'%label
+                output += str(value)
+                output += ' '
+
         return output
 
 
     def to_json(self):
-        labels = ['line_str', 'split_tokens', 'datetime', 'operation', \
-                  'connection', 'namespace', 'nscanned', 'ntoreturn', \
-                  'nreturned', 'duration']
-        variables = vars(self)
+        """ converts LogLine object to valid JSON. """
         output = {}
+        labels = ['line_str', 'split_tokens', 'datetime', 'operation', \
+                  'connection', 'namespace', 'nscanned', 'ntoreturn',  \
+                  'nreturned', 'ninserted', 'nupdated', 'duration']
 
         for label in labels:
-            if (not label in variables) or (variables[label] == None):
-                continue
-            output[label] = variables[label]
+            value = getattr(self, label, None)
+            if value != None:
+                output[label] = value
 
-        return json.dumps(output, default=self.dthandler)
+        return json.dumps(output, cls=DateTimeEncoder)
+
+
+
 
 
