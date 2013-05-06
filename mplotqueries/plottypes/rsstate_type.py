3abd32b807ee78e57cc42f0f65f309d05d7da3d8
Thomas Rueckstiess <thomas@10gen.com>
2013-05-06 20:46:55 +1000

    fixed typo and cleaned up a bit more.

--- mplotqueries/plottypes/rsstate_type.py
+++ mplotqueries/plottypes/rsstate_type.py
@@ -3,7 +3,7 @@ from event_type import EventPlotType
 class RSStatePlotType(EventPlotType):
     plot_type_str = 'rsstate'
 
-    # force group() to always use lastword method to groupy by
+    # force group() to always use lastword method to group by
     group_by = 'lastword'
 
     colors = ['m', 'y', 'r', 'g', 'g', 'k', 'b', 'c']
@@ -21,13 +21,15 @@ class RSStatePlotType(EventPlotType):
 
 
     def lastword(self, logline):
-        """ group by the last message of the log line (PRIMARY, SECONDARY, ...) """
+        """ group by the last token of the log line (PRIMARY, SECONDARY, ...) """
         return logline.split_tokens[-1]
 
 
     def accept_line(self, logline):
-        """ only match log lines containing 'is now in state' (others' state changes) or from rsMgr task (own state changes). """
-        if "is now in state" in logline.line_str:
+        """ only match log lines containing 'is now in state' (reflects other node's state changes) 
+            or of type "[rsMgr] replSet PRIMARY" (reflects own state changes). 
+        """
+        if "is now in state" in logline.line_str and logline.split_tokens[-1] in self.states:
             return True
 
         if "replSet" in logline.line_str and logline.thread == "rsMgr" and logline.split_tokens[-1] in self.states:
