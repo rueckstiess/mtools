0714c7c618352d9321f32309d7ff1238da83a204
Thomas Rueckstiess <thomas@10gen.com>
2013-07-05 13:03:46 +1000

    some matplotqueries refactoring. Added scatter plot type, nscanned/n plot type. Layout changes. Higher DPI. fixes #58.

--- mtools/mplotqueries/plottypes/__init__.py
+++ mtools/mplotqueries/plottypes/__init__.py
@@ -1,4 +1,3 @@
-from duration_type import DurationPlotType
-from event_type import EventPlotType
+from scatter_type import ScatterPlotType, DurationPlotType, NScannedNPlotType
+from event_type import EventPlotType, RSStatePlotType
 from range_type import RangePlotType
-from rsstate_type import RSStatePlotType
