7f708205c638be27a0f426a1bca1fb9d75130e12
Gianfranco Palumbo <gianpa@gmail.com>
2013-09-13 10:10:59 +0100

    Feature #95 Allow to input multiple files to mloginfo

--- mtools/mloginfo/mloginfo.py
+++ mtools/mloginfo/mloginfo.py
@@ -13,8 +13,8 @@ class MLogInfoTool(LogFileTool):
 
     def __init__(self):
         """ Constructor: add description to argparser. """
-        LogFileTool.__init__(self, multiple_logfiles=False, stdin_allowed=False)
-        
+        LogFileTool.__init__(self, multiple_logfiles=True, stdin_allowed=False)
+
         self.argparser.description = 'Extracts general information from logfile and prints it to stdout.'
         self.argparser.add_argument('--verbose', action='store_true', help='show more verbose output (depends on info section)')
         self.argparser_sectiongroup = self.argparser.add_argument_group('info sections', 'Below commands activate additional info sections for the log file.')
@@ -22,50 +22,53 @@ class MLogInfoTool(LogFileTool):
         # add all filter classes from the filters module
         self.sections = [c[1](self) for c in inspect.getmembers(sections, inspect.isclass)]
 
-
     def run(self, arguments=None):
         """ Print out useful information about the log file. """
         LogFileTool.run(self, arguments)
 
-        self.logfile = LogFile(self.args['logfile'])
-        print "start of logfile: %s" % (self.logfile.start.strftime("%b %d %H:%M:%S") if self.logfile.start else "unknown")
-        print "  end of logfile: %s" % (self.logfile.end.strftime("%b %d %H:%M:%S") if self.logfile.start else "unknown")
+        self.logfiles = self.args['logfile']
 
-        # get one logline (within first 20 lines) for datetime format
-        logline = None
-        for i in range(20):
-            try:
-                logline = LogLine(self.args['logfile'].next())
-            except StopIteration as e:
-                raise SystemExit("no valid log lines found (datetime not available).")
-            if logline.datetime:
-                break
+        for logfileOpen in self.args['logfile']:
+            self.logfile = LogFile(logfileOpen)
 
-        # TODO: add timezone if iso8601 format
-        
-        print "    line numbers: %s" % self.logfile.num_lines
-        print "          binary: %s" % (self.logfile.binary or "unknown")
+            print "        filename: %s" % self.args['logfile'][0].name
+            print "start of logfile: %s" % (self.logfile.start.strftime("%b %d %H:%M:%S") if self.logfile.start else "unknown")
+            print "  end of logfile: %s" % (self.logfile.end.strftime("%b %d %H:%M:%S") if self.logfile.start else "unknown")
 
-        version = (' -> '.join(self.logfile.versions) or "unknown")
-        
-        # if version is unknown, go by date
-        if version == 'unknown' and logline:
-            if logline.datetime_format == 'ctime-pre2.4':
-                version = '< 2.4 (no milliseconds)'
-            elif logline.datetime_format == 'ctime':
-                version = '>= 2.4 (milliseconds present)'
-            elif logline.datetime_format.startswith('iso8601-'):
-                version = '>= 2.6 (iso8601 format)'
+            # get one logline (within first 20 lines) for datetime format
+            logline = None
+            for i in range(20):
+                try:
+                    logline = LogLine(logfileOpen.next())
+                except StopIteration as e:
+                    raise SystemExit("no valid log lines found (datetime not available).")
+                if logline.datetime:
+                    break
 
-        print "         version: %s" % version
-        print 
+            # TODO: add timezone if iso8601 format
 
-        # now run all sections
-        for section in self.sections:
-            if section.active:
-                print
-                print section.name.upper()
-                section.run()
+            print "    line numbers: %s" % self.logfile.num_lines
+            print "          binary: %s" % (self.logfile.binary or "unknown")
+
+            version = (' -> '.join(self.logfile.versions) or "unknown")
+
+            # if version is unknown, go by date
+            if version == 'unknown' and logline:
+                if logline.datetime_format == 'ctime-pre2.4':
+                    version = '< 2.4 (no milliseconds)'
+                elif logline.datetime_format == 'ctime':
+                    version = '>= 2.4 (milliseconds present)'
+                elif logline.datetime_format.startswith('iso8601-'):
+                    version = '>= 2.6 (iso8601 format)'
+
+            print "         version: %s" % version,
+
+            # now run all sections
+            for section in self.sections:
+                if section.active:
+                    print
+                    print section.name.upper()
+                    section.run()
 
 
 if __name__ == '__main__':
