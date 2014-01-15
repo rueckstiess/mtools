45e98bb4736c3e3bc92ae36d028507c53b3075c6
Thomas Rueckstiess <thomas@rueckstiess.net>
2014-01-15 15:01:43 +1100

    moved cluster functionality to mlaunch.py.
    too specific to be separate, also had problems with circular imports.

--- mtools/mlaunch/mlaunch.py
+++ mtools/mlaunch/mlaunch.py
@@ -7,8 +7,11 @@ import socket
 import json
 import re
 
+from collections import defaultdict
+from operator import itemgetter
+
 from mtools.util.cmdlinetool import BaseCmdLineTool
-from mtools.util.cluster import Cluster
+
 
 try:
     try:
@@ -60,6 +63,12 @@ class MLaunchTool(BaseCmdLineTool):
         self.hostname = socket.gethostname()
         
         self.startup_info = {}
+
+        # data structures for the discovery feature
+        self.cluster_tree = {}
+        self.cluster_tags = defaultdict(list)
+        self.cluster_running = {}
+
 
         self.argparser.description = 'script to launch MongoDB stand-alone servers, replica sets and shards. You must specify either --single or --replicaset. \
             In addition to the optional arguments below, you can specify any mongos and mongod argument, which will be passed on, if the process accepts it.'
@@ -168,6 +177,15 @@ class MLaunchTool(BaseCmdLineTool):
         # todo
 
 
+    def list(self):
+        # ---DEBUG---
+        if not self.load_parameters():
+            raise SystemExit("can't read %s/.mlaunch_startup. Is this an mlaunch'ed cluster?" % self.dir)
+
+        self.discover()
+        print self.cluster_running
+
+
     def start(self):
         # check if authentication is enabled, make key file       
         if self.args['authentication']:
@@ -237,6 +255,178 @@ class MLaunchTool(BaseCmdLineTool):
             print 'creating directory: %s'%dbpath
         
         return datapath
+
+
+
+    def discover(self):
+        
+        # get shard names
+        shard_names = self._getShardNames()
+
+        # determine number of nodes to inspect
+        if 'sharded' in self.args and self.args['sharded'] != None:
+            num_config = self.args['config']
+            num_mongos = self.args['mongos']
+            num_shards = len(shard_names)
+        else:
+            num_shards = 1
+            num_config = 0
+            num_mongos = 0
+
+        num_nodes_per_shard = self.args['nodes'] if 'replicaset' in self.args and self.args['replicaset'] else 1
+        if 'arbiter' in self.args and self.args['arbiter']:
+            num_nodes_per_shard += 1
+
+        num_nodes = num_shards * num_nodes_per_shard + num_config + num_mongos
+
+        current_port = self.args['port']
+
+        # tag all nodes with 'all'
+        self.cluster_tags['all'].extend ( range(current_port, current_port + num_nodes) )
+
+        # find all mongos
+        for i in range(num_mongos):
+            port = i+current_port
+
+            try:
+                mc = Connection( 'localhost:%i'%port )
+                # if this is not a mongos, something went wrong, fail
+                assert (mc.is_mongos)
+                running = True
+
+            except ConnectionFailure:
+                # node not reachable
+                running = False
+
+            # add mongos to cluster tree
+            self.cluster_tree.setdefault( 'mongos', [] ).append( port )
+            # add mongos to tags
+            self.cluster_tags['mongos'].append( port )
+            self.cluster_tags['running' if running else 'down'].append( port )
+            # add mongos to running map
+            self.cluster_running[port] = running
+
+        current_port += num_mongos
+
+        # find all mongods (sharded, replicaset or single)
+        if shard_names == None:
+            shard_names = [ None ]
+
+        for shard in shard_names:
+            port_range = range(current_port, current_port + num_nodes_per_shard)
+
+            # all of these are mongod nodes
+            self.cluster_tags['mongod'].extend( port_range )
+
+            if shard:
+                # if this is a shard, store in cluster_tree and tag shard name
+                self.cluster_tree.setdefault( 'shard', [] ).append( port_range )
+                self.cluster_tags[shard].extend( port_range )
+
+            if 'replicaset' in self.args and self.args['replicaset']:
+                # treat replica set as a whole
+                rs_name = shard if shard else self.args['replset']
+                try:
+                    mrsc = ReplicaSetConnection( ','.join( 'localhost:%i'%i for i in port_range ), replicaSet=rs_name )
+                    # primary, secondaries, arbiters
+                    self.cluster_tags['primary'].append( mrsc.primary[1] )
+                    self.cluster_tags['secondary'].extend( map(itemgetter(1), mrsc.secondaries) )
+                    self.cluster_tags['arbiter'].extend( map(itemgetter(1), mrsc.arbiters) )
+
+                    # split by secondary position
+                    self.cluster_tree.setdefault( 'secondary', [] )
+                    for i, secondary in enumerate(map(itemgetter(1), mrsc.secondaries)):
+                        if len(self.cluster_tree['secondary']) <= i:
+                            self.cluster_tree['secondary'].append([])
+                        self.cluster_tree['secondary'][i].append(secondary)
+
+                except ConnectionFailure:
+                    # none of the nodes of the replica set is running, mark down then next shard
+                    self.cluster_tags['down'].extend( port_range )
+                    continue
+
+            elif 'single' in self.args and self.args['single']:
+                self.cluster_tags['single'].append( current_port )
+
+            # now determine which nodes are running / down
+            for i in range(num_nodes_per_shard):
+                port = i+current_port
+                
+                try:
+                    mc = Connection( 'localhost:%i'% port )
+                    running = True
+
+                except ConnectionFailure:
+                    # node not reachable
+                    running = False
+
+                # add mongod to tags
+                self.cluster_tags['running' if running else 'down'].append( port )
+
+                # add node to running map
+                self.cluster_running[port] = running
+
+            # increase current_port
+            current_port += num_nodes_per_shard
+
+
+        # find all config servers
+        for i in range(num_config):
+            port = i+current_port
+
+            try:
+                mc = Connection( 'localhost:%i'%port )
+                # if this is not a config server, something went wrong, fail
+                assert (u'config' in mc.database_names())
+                running = True
+
+            except ConnectionFailure:
+                # node not reachable
+                running = False
+
+            # add config server to cluster tree
+            self.cluster_tree.setdefault( 'config', [] ).append( port )
+            # add config server to tags
+            self.cluster_tags['config'].append( port )
+            self.cluster_tags['running' if running else 'down'].append( port )
+            # add config server to running map
+            self.cluster_running[port] = running
+
+        current_port += num_mongos
+
+
+
+    def is_running(self, port):
+        return self.cluster_running[port]
+
+
+    def get_tagged(self, tags):
+        """ The format for the tags list is tuples for tags: mongos, config, shard, secondary tags
+            of the form (tag, number), e.g. ('mongos', 2) which references the second mongos 
+            in the list. For all other tags, it is simply the string, e.g. 'primary'.
+        """
+
+        nodes = set(self.cluster_tags['all'])
+
+        for tag in tags:
+            if type(tag) == tuple:
+                # special case for tuple tags: mongos, config, shard. These can contain a number
+                tag, number = tag
+                assert (tag in ('mongos', 'config', 'shard'))
+
+                branch = self.cluster_tree[tag][number]
+                if hasattr(branch, '__iter__'):
+                    subset = set(branch)
+                else:
+                    subset = set([branch])
+            else:
+                # otherwise use tags dict to get the subset
+                subset = set(self.cluster_tags[tag])
+
+            nodes = nodes.intersection(subset)
+
+        return nodes
+
 
 
     def _filter_valid_arguments(self, arguments, binary="mongod"):
