15edf855c0bce9acb81a9c1f788ecfb3939a1498
corymintz <cory.mintz@10gen.com>
2014-05-21 12:39:03 -0400

    Support undocumented args

--- mtools/mlaunch/mlaunch.py
+++ mtools/mlaunch/mlaunch.py
@@ -147,8 +147,12 @@ def shutdown_host(port, username=None, password=None, authdb=None):
 
 class MLaunchTool(BaseCmdLineTool):
 
+    UNDOCUMENTED_MONGOD_ARGS = ['--nopreallocj']
+    UNDOCUMENTED_MONGOS_ARGS = []
+
     def __init__(self, test=False):
         BaseCmdLineTool.__init__(self)
+        self.hostname = socket.gethostname()
 
         # arguments
         self.args = None
@@ -1483,20 +1487,22 @@ class MLaunchTool(BaseCmdLineTool):
         result = []
         for i, arg in enumerate(arguments):
             if arg.startswith('-'):
-                # check if the binary accepts this argument or special
-                # case -vvv for any number of v
+                # check if the binary accepts this argument or special case -vvv for any number of v
                 if arg in accepted_arguments or re.match(r'-v+', arg):
                     result.append(arg)
-                else:
+                elif binary.endswith('mongod') and arg in self.UNDOCUMENTED_MONGOD_ARGS:
+                    result.append(arg)
+                elif binary.endswith('mongos') and arg in self.UNDOCUMENTED_MONGOS_ARGS:
+                    result.append(arg)
+                elif self.ignored_arguments.get(binary + arg) is None:
                     # warn once for each combination of binary and unknown arg
-                    if self.ignored_arguments.get(binary + arg) is None:
-                        self.ignored_arguments[binary + arg] = True
-                        print("warning: ignoring unknown argument %s for %s"
-                              % (arg, binary))
+                    self.ignored_arguments[binary + arg] = True
+                    print("warning: ignoring unknown argument %s for %s" % (arg, binary))
             elif i > 0 and arguments[i - 1] in result:
                 # if it doesn't start with a '-', it could be the value of
                 # the last argument, e.g. `--slowms 1000`
                 result.append(arg)
+
 
         # return valid arguments as joined string
         return ' '.join(result)
