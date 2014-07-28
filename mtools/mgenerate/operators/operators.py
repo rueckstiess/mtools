ca5c97a209dada4e8657e2531f98d7950fb45a68
Thomas Rueckstiess <thomas@rueckstiess.net>
2014-07-28 19:53:20 -0400

    added  alias

--- mtools/mgenerate/operators/operators.py
+++ mtools/mgenerate/operators/operators.py
@@ -203,7 +203,7 @@ class CoordinateOperator(BaseOperator):
 
     dict_format = True
     string_format = True
-    names = ['$coordinates', '$coord']
+    names = ['$coordinates', '$coordinate', '$coord']
     defaults = OrderedDict([ ('long_lim', [-180, 180]), ('lat_lim', [-90, 90]) ])
 
     def __call__(self, options=None):
