0e25dfec90da1cc3825024df76c4161589f9992b
Jim OLeary <jim.oleary@gmail.com>
2014-10-12 21:20:31 +0100

    added 2.8 level and component

--- mtools/mlogfilter/filters/logline_filter.py
+++ mtools/mlogfilter/filters/logline_filter.py
@@ -3,10 +3,15 @@ from mtools.util.pattern import json2pattern
 import re
 from base_filter import BaseFilter
 
+def custom_parse_array(value):
+    return list(set(value.split()))
+
 class LogLineFilter(BaseFilter):
     """ 
     """
     filterArgs = [
+        ('--component', {'action':'store', 'nargs':'*', 'metavar':'CM', 'help':'only output log lines matching operations on CM (multiple values are allowed).'}),
+        ('--level',     {'action':'store', 'nargs':'*', 'metavar':'LL', 'help':'only output log lines matching operations on LL (multiple values are allowed).'}),
         ('--namespace', {'action':'store', 'metavar':'NS', 'help':'only output log lines matching operations on NS.'}),
         ('--operation', {'action':'store', 'metavar':'OP', 'help':'only output log lines matching operations of type OP.'}),
         ('--thread',    {'action':'store', 'help':'only output log lines of thread THREAD.'}),
@@ -16,11 +21,19 @@ class LogLineFilter(BaseFilter):
     def __init__(self, mlogfilter):
         BaseFilter.__init__(self, mlogfilter)
 
-        self.namespace = None
-        self.operation = None
-        self.thread = None
-        self.pattern = None
+        self.components = None
+        self.levels     = None
+        self.namespace  = None
+        self.operation  = None
+        self.thread     = None
+        self.pattern    = None
 
+        if 'component' in self.mlogfilter.args and self.mlogfilter.args['component']:
+            self.components = custom_parse_array(self.mlogfilter.args['component'])
+            self.active = True
+        if 'level' in self.mlogfilter.args and self.mlogfilter.args['level']:
+            self.levels = custom_parse_array(self.mlogfilter.args['level'])
+            self.active = True
         if 'namespace' in self.mlogfilter.args and self.mlogfilter.args['namespace']:
             self.namespace = self.mlogfilter.args['namespace']
             self.active = True
@@ -37,6 +50,10 @@ class LogLineFilter(BaseFilter):
     def accept(self, logevent):
         # if several filters are active, all have to agree
         res = False
+        if self.components and logevent.component not in self.components:
+            return False
+        if self.levels and logevent.level not in self.levels:
+            return False
         if self.namespace and logevent.namespace != self.namespace:
             return False
         if self.operation and logevent.operation != self.operation:
