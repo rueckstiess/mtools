4ac1d6704d477c00bd37300a125b9688f3bd6fb7
Stephen Steneker <stennie@cpan.org>
2017-09-21 13:35:37 +1000

    Fix #509: Add mplotqueries --type docsExamined/n

--- mtools/mplotqueries/plottypes/__init__.py
+++ mtools/mplotqueries/plottypes/__init__.py
@@ -1,4 +1,4 @@
-from scatter_type import ScatterPlotType, NScannedNPlotType, DurationLineType
+from scatter_type import ScatterPlotType, NScannedNPlotType, DocsExaminedPlotType, DurationLineType
 from event_type import EventPlotType, RSStatePlotType
 from range_type import RangePlotType
 from histogram_type import HistogramPlotType
