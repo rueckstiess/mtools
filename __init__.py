d022ced768fc348c5b0004d229cfcb3165e18f65
garycahill <46780675+garycahill@users.noreply.github.com>
2019-05-28 17:10:09 +0100

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from .restart_section import RestartSection
-from .distinct_section import DistinctSection
-from .connection_section import ConnectionSection
-from .query_section import QuerySection
-from .rs_state_section import RsStateSection
-from .rs_info_section import RsInfoSection
-from .clients_section import ClientSection
