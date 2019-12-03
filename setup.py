e2e81c4dc31a3f18e7ca5fc39445aa0e7b0e3353
Stephen Steneker <stennie@cpan.org>
2019-12-04 08:15:48 +1100

    Fix #738: Require minimum dependency versions rather than exact

--- setup.py
+++ setup.py
@@ -10,19 +10,19 @@ try:
     from setuptools import setup, find_packages
 
     # test for 2.7-included packages, add to requirements if not available
-    install_requires = ['six', 'python-dateutil==2.7']
+    install_requires = ['six', 'python-dateutil>=2.7']
 
     # Additional dependencies from requirements.txt that should be installed
     # for full mtools feature support. These are optional dependencies to
     # simplify the default install experience, particularly where a build
     # toolchain is required.
     extras_requires = {
-        "all": ['matplotlib==3.1.1', 'numpy==1.16.4', 'pymongo==3.8.0', 'psutil==5.6.3'],
-        "mlaunch": ['pymongo==3.8.0', 'psutil==5.6.3'],
+        "all": ['matplotlib>=3.1.1', 'numpy>=1.16.4', 'pymongo>=3.8.0', 'psutil>=5.6.3'],
+        "mlaunch": ['pymongo>=3.8.0', 'psutil>=5.6.3'],
         "mlogfilter": [],
-        "mloginfo": ['numpy==1.16.4'],
+        "mloginfo": ['numpy>=1.16.4'],
         "mlogvis": [],
-        "mplotqueries": ['matplotlib==3.1.1', 'numpy==1.16.4'],
+        "mplotqueries": ['matplotlib>=3.1.1', 'numpy>=1.16.4'],
     }
 
     try:
