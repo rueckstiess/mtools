eed98436b431d4866cad28dee4d43ffb4494e22f
Stephen Steneker <stennie@cpan.org>
2019-12-20 17:23:56 +1100

    Skip interactive mplotqueries test

--- mtools/test/test_mplotqueries.py
+++ mtools/test/test_mplotqueries.py
@@ -4,7 +4,8 @@ import os
 import mtools
 from mtools.mplotqueries.mplotqueries import MPlotQueriesTool
 from mtools.util.logfile import LogFile
-
+from nose.plugins.skip import SkipTest
+from nose.tools import raises
 
 class TestMPlotQueries(object):
 
@@ -19,6 +20,7 @@ class TestMPlotQueries(object):
         self.logfile = LogFile(open(self.logfile_path, 'rb'))
 
     def test_checkpoints(self, filename='mongod_4.0.10_slowcheckpoints.log'):
+        raise SkipTest('Skipping interactive test')
         # different logfile for the slow Checkpoints
         self.logfile_path = os.path.join(os.path.dirname(mtools.__file__), 'test/logfiles/', filename)
         self.tool.run('%s --checkpoints' % self.logfile_path)
