b77d8c60c8331c306b932669269cf21ee7994ebd
Stephen Steneker <stennie@cpan.org>
2020-02-09 17:22:42 +1100

    Remove unimplemented option

--- mtools/mlaunch/mlaunch.py
+++ mtools/mlaunch/mlaunch.py
@@ -328,9 +328,6 @@ class MLaunchTool(BaseCmdLineTool):
                                        'required to run the stop command '
                                        '(requires --auth, default="%s")'
                                        % ' '.join(self._default_auth_roles)))
-        init_parser.add_argument('--auth-role-docs', action='store_true',
-                                 default=False,
-                                 help='auth-roles are json documents')
         init_parser.add_argument('--no-initial-user', action='store_false',
                                  default=True, dest='initial-user',
                                  help=('create an initial user if auth is '
