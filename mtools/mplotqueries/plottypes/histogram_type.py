58976c8c2bf37faa976a854c3fed08261bf371ce
Stephen Steneker <git@stennie.dev>
2020-10-14 19:56:36 +1100

    Fix flake8 complaints

--- mtools/mplotqueries/plottypes/histogram_type.py
+++ mtools/mplotqueries/plottypes/histogram_type.py
@@ -7,11 +7,11 @@ from mtools.util.log2code import Log2CodeConverter
 
 try:
     from matplotlib.dates import date2num, num2date
-except ImportError:
+except ImportError as error:
     raise ImportError("Can't import matplotlib. See "
                       "https://matplotlib.org/users/installing.html "
                       "for instructions on how to install matplotlib."
-                      "Error: " + str(e))
+                      "Error: " + str(error))
 
 
 class HistogramPlotType(BasePlotType):
